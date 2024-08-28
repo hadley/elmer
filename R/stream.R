8b1857570ea5ae7a4c73b0880f63c4a2b5a2a692
Hadley Wickham <h.wickham@gmail.com>
2024-08-28 09:27:06 -0500

    Use req_stream_connection()

--- R/stream.R
+++ R/stream.R
@@ -1,42 +1,22 @@
 # Want callback for each message too?
-req_perform_chat <- function(req,
-                             messages,
-                             stream = identity) {
+req_perform_chat <- function(req, messages) {
 
   force(messages)
   force(stream)
   results <- list()
+  resp <- httr2::req_perform_connection(req)
 
-  callback <- function(bytes) {
-    sse_text <- rawToChar(bytes)
-    sse_messages <- strsplit(sse_text, "\n\n")[[1]]
-    sse_lines <- strsplit(sse_messages, "\n")
-
-    # shouldn't have messages with multiple lines > 1
-    if (any(lengths(sse_lines) > 1)) {
-      abort("Malformed stream", .internal = TRUE)
+  repeat({
+    event <- httr2::resp_stream_sse(resp)
+    if (is.null(event) || event$data == "[DONE]") {
+      break
     }
-    lines <- unlist(sse_lines)
-    is_data <- grepl("^data: ", lines)
+    json <- jsonlite::parse_json(event$data)
+    cat(json$choices[[1]]$delta$content)
 
-    # should only be data messages
-    if (!all(is_data)) {
-      abort("Malformed stream", .internal = TRUE)
-    }
-    contents <- sub("^data: ", "", lines)
+    results <<- merge_dicts(results, json)
+  })
 
-    is_done <- any(contents == "[DONE]")
-    contents <- contents[contents != "[DONE]"]
-    jsons <- lapply(contents, jsonlite::parse_json)
-
-    for (json in jsons) {
-      results <<- merge_dicts(results, json)
-      stream(json$choices[[1]]$delta)
-    }
-
-    TRUE
-  }
-  resp <- httr2::req_perform_stream(req, callback, wait_for = 0, round = "sse")
 
   result_message1 <- results$choices[[1]]$delta # $message if not streaming
   messages <- c(messages, list(result_message1), call_tools(result_message1))
