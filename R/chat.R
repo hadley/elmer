056b8c9af66e23596371f7e3fa7152558518b061
Joe Cheng <joe@posit.co>
2024-09-16 10:47:23 -0700

    Document content_image_url and content_image_file

--- R/chat.R
+++ R/chat.R
@@ -541,15 +541,28 @@ print.ChatOpenAI <- function(x, ...) {
   invisible(x)
 }
 
+#' Encode image content for chat input
+#'
+#' These functions are used to prepare image URLs and files for input to the
+#' chatbot. The `content_image_url()` function is used to provide a URL to an
+#' image, while `content_image_file()` is used to provide the image data itself.
+#'
+#' @param url The URL of the image to include in the chat input. Can be a
+#'   `data:` URL or a regular URL. Valid image types are PNG, JPEG, WebP, and
+#'   non-animated GIF.
+#' @param detail The [detail
+#'   setting](https://platform.openai.com/docs/guides/vision/low-or-high-fidelity-image-understanding)
+#'   for this image. Can be `"auto"`, `"low"`, or `"high"`.
+#' @returns An input object suitable for including in the `...` parameter of
+#'   the `chat()`, `stream()`, `chat_async()`, or `stream_async()` methods.
+#'
 #' @export
 #' @examplesIf elmer:::openai_key_exists()
 #' chat <- new_chat_openai(echo = TRUE)
 #' chat$chat(
-#'   list(
-#'     "What do you see in these images?",
-#'     content_image_url("https://www.r-project.org/Rlogo.png"),
-#'     content_image_file(system.file("httr2.png", package = "elmer"))
-#'   )
+#'   "What do you see in these images?",
+#'   content_image_url("https://www.r-project.org/Rlogo.png"),
+#'   content_image_file(system.file("httr2.png", package = "elmer"))
 #' )
 content_image_url <- function(url, detail = c("auto", "low", "high")) {
   # TODO: Allow vector input?
@@ -559,13 +572,18 @@ content_image_url <- function(url, detail = c("auto", "low", "high")) {
 }
 
 #' @rdname content_image_url
+#' @param path The path to the image file to include in the chat input. Valid
+#'   file extensions are `.png`, `.jpeg`, `.jpg`, `.webp`, and (non-animated)
+#'   `.gif`.
+#' @param content_type The content type of the image (e.g. `image/png`). If
+#'   `"auto"`, the content type is inferred from the file extension.
 #' @param resize If `low`, resize images to fit within 512x512. If `high`,
 #'   resize to fit within 2000x768 or 768x2000. If `FALSE`, do not resize. You
 #'   can also pass a custom string to resize the image to a specific size, e.g.
 #'   `"200x200"` to resize to 200x200 pixels while preserving aspect ratio.
 #'   Append `>` to resize only if the image is larger than the specified size,
-#'   and `!` to ignore aspect ratio (e.g. `"300x200>!"`).
-#'   All values other than `FALSE` require the `magick` package.
+#'   and `!` to ignore aspect ratio (e.g. `"300x200>!"`). All values other than
+#'   `FALSE` require the `magick` package.
 #' @export
 content_image_file <- function(path, content_type = "auto", resize = "low") {
   # TODO: Allow vector input?
