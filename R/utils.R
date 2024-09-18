8e673df00295463c4552a7b616e01fb1e5bc5fc6
Hadley Wickham <h.wickham@gmail.com>
2024-09-18 16:41:40 -0500

    Fix get_key() implementation

--- R/utils.R
+++ R/utils.R
@@ -2,16 +2,15 @@ is_testing <- function() {
   identical(Sys.getenv("TESTTHAT"), "true")
 }
 
-get_key <- function(name) {
+get_key <- function(name, error_call = caller_env()) {
   val <- Sys.getenv(name)
-  if (!identical(val, name)) {
+  if (!identical(val, "")) {
     val
   } else {
     if (is_testing()) {
       testthat::skip(sprintf("%s env var is not configured", name))
     } else {
-      cli::cli_abort("Can't find env var {.code {name}}.")
+      cli::cli_abort("Can't find env var {.code {name}}.", call = error_call)
     }
   }
 }
-
