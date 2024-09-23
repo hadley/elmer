7abc7b1f44aaafd85f08ce8c09bf31b914ac0934
Hadley Wickham <h.wickham@gmail.com>
2024-09-23 16:43:59 -0500

    Tease apart chat (#35)
    * Start teasing apart Chat class from OpenAI
    * Improve function names
    * Prepare openai_chat_request to become a generic
    * Switch to S7 & move more code to constructor

--- R/api-openai.R
+++ R/api-openai.R
@@ -1,122 +1,183 @@
-openai_model <- function(base_url = "https://api.openai.com/v1",
-                         model = "gpt-4o-mini",
-                         seed = NULL,
-                         extra_args = list(),
-                         api_key = openai_key()) {
-  structure(
-    list(
-      base_url = base_url,
-      model = model,
-      seed = seed,
-      api_key = api_key,
-      extra_args = list()
-    ),
-    class = "elmer::openai_model"
-  )
-}
 
-
-openai_chat <- function(mode = c("value", "stream", "async-stream", "async-value"),
-                        model,
-                        messages,
-                        tools = list()) {
-
-  mode <- arg_match(mode)
-  stream <- mode %in% c("stream", "async-stream")
-
-  req <- openai_chat_req(
-    messages = messages,
-    tools = tools,
-    model = model$model,
-    seed = model$seed,
-    stream = stream,
-    base_url = model$base_url,
-    api_key = model$api_key
-  )
-
-  handle_response <- switch(mode,
-    "value" = function(req) resp_body_json(req_perform(req)),
-    "async-value" = function(req) promises::then(req_perform_promise(req), resp_body_json),
-    "stream" = openai_chat_stream,
-    "async-stream" = openai_chat_stream_async
-  )
-
-  handle_response(req)
-}
-
-openai_chat_is_done <- function(event) {
-  identical(event$data, "[DONE]")
-}
-
-openai_chat_parse <- function(event) {
-  jsonlite::parse_json(event$data)
-}
-
-on_load(openai_chat_stream <- chat_stream(openai_chat_is_done, openai_chat_parse))
-on_load(openai_chat_stream_async <- chat_stream_async(openai_chat_is_done, openai_chat_parse))
-
-openai_chunk_text <- function(event, streaming) {
-  if (streaming) {
-    event$choices[[1]]$delta$content
-  } else {
-    event$choices[[1]]$message$content
-  }
-}
-openai_merge_chunks <- function(cur, chunk) {
-  if (is.null(cur)) {
-    chunk
-  } else {
-    merge_dicts(cur, chunk)
-  }
-}
-openai_result_message <- function(result, streaming) {
-  if (streaming) {
-    result$choices[[1]]$delta
-  } else {
-    result$choices[[1]]$message
-  }
-}
-
-# https://platform.openai.com/docs/api-reference/chat/create
-openai_chat_req <- function(messages,
-                            tools = list(),
-                            model = "gpt-4o-mini",
-                            seed = NULL,
-                            stream = TRUE,
-                            extra_args = list(),
+#' Create a chatbot that speaks to an OpenAI compatible endpoint
+#'
+#' This function returns a [Chat] object that takes care of managing the state
+#' associated with the chat; i.e. it records the messages that you send to the
+#' server, and the messages that you receive back. If you register a tool
+#' (aka an R function), it also takes care of the tool loop.
+#'
+#' @param system_prompt A system prompt to set the behavior of the assistant.
+#' @param messages A list of messages to start the chat with (i.e., continuing a
+#'   previous conversation). If not provided, the conversation begins from
+#'   scratch. Do not provide non-`NULL` values for both `messages` and
+#'   `system_prompt`.
+#'
+#'   Each message in the list should be a named list with at least `role`
+#'   (usually `system`, `user`, or `assistant`, but `tool` is also possible).
+#'   Normally there is also a `content` field, which is a string.
+#' @param base_url The base URL to the endpoint; the default uses OpenAI.
+#' @param api_key The API key to use for authentication. You generally should
+#'   not supply this directly, but instead set the `OPENAI_API_KEY` environment
+#'   variable.
+#' @param model The model to use for the chat; set to `NULL` (the default) to
+#'   use a reasonable model, currently `gpt-4o-mini`. We strongly recommend
+#'   explicitly choosing a model for all but the most casual use.
+#' @param seed Optional integer seed that ChatGPT uses to try and make output
+#'   more reproducible.
+#' @param api_args Named list of arbitrary extra arguments passed to every
+#'   chat API call.
+#' @param echo If `TRUE`, the `chat()` method streams the response to stdout by
+#'   default. (Note that this has no effect on the `stream()`, `chat_async()`,
+#'   and `stream_async()` methods.)
+#' @export
+#' @value A [Chat] object
+#' @examplesIf elmer:::openai_key_exists()
+#' chat <- new_chat_openai()
+#' chat$chat("
+#'   What is the difference between a tibble and a data frame?
+#'   Answer with a bulleted list
+#' ")
+#'
+#' chat <- new_chat_openai()
+#' chat$register_tool(
+#'   fun = rnorm,
+#'   name = "rnorm",
+#'   description = "Drawn numbers from a random normal distribution",
+#'   arguments = list(
+#'     n = tool_arg(
+#'       type = "integer",
+#'       description = "The number of observations. Must be a positive integer."
+#'     ),
+#'     mean = tool_arg(
+#'       type = "number",
+#'       description = "The mean value of the distribution."
+#'     ),
+#'     sd = tool_arg(
+#'       type = "number",
+#'       description = "The standard deviation of the distribution. Must be a non-negative number."
+#'     )
+#'   )
+#' )
+#' chat$chat("
+#'   Give me five numbers from a random normal distribution.
+#'   Briefly explain your work.
+#' ")
+new_chat_openai <- function(system_prompt = NULL,
+                            messages = NULL,
                             base_url = "https://api.openai.com/v1",
-                            api_key = openai_key()) {
+                            api_key = openai_key(),
+                            model = NULL,
+                            seed = NULL,
+                            api_args = list(),
+                            echo = FALSE) {
+  check_string(system_prompt, allow_null = TRUE)
+  openai_check_conversation(messages, allow_null = TRUE)
+  check_bool(echo)
 
-  check_string(model)
-  check_number_whole(seed, allow_null = TRUE)
-  check_bool(stream)
-
-  data <- compact(list2(
-    messages = messages,
+  model <- new_openai_model(
+    base_url = base_url,
     model = model,
     seed = seed,
-    stream = stream,
-    tools = tools,
-    !!!extra_args
-  ))
+    extra_args = api_args,
+    api_key = api_key
+  )
 
-  req <- openai_request(base_url = base_url, key = api_key)
-  req <- req_url_path_append(req, "/chat/completions")
-  req <- req_body_json(req, data)
-  req
+  messages <- openai_apply_system_prompt(system_prompt, messages)
+  Chat$new(model = model, messages = messages, echo = echo)
 }
 
-openai_request <- function(base_url = "https://api.openai.com/v1",
-                           key = openai_key()) {
-  req <- request(base_url)
-  req <- req_auth_bearer_token(req, Sys.getenv("OPENAI_API_KEY"))
-  req <- req_retry(req, max_tries = 2)
-  req <- req_error(req, body = function(resp) {
-     resp_body_json(resp)$error$message
-  })
-  # req <- req_verbose(req, body_req = TRUE, body_resp = TRUE)
-  req
+openai_apply_system_prompt <- function(system_prompt, messages) {
+  if (is.null(system_prompt)) {
+    return(messages)
+  }
+
+  system_prompt_message <- list(
+    role = "system",
+    content = system_prompt
+  )
+
+  # No messages; start with just the system prompt
+  if (length(messages) == 0) {
+    return(list(system_prompt_message))
+  }
+
+  # No existing system prompt message; prepend the new one
+  if (messages[[1]][["role"]] != "system") {
+    return(c(list(system_prompt_message), messages))
+  }
+
+  # Duplicate system prompt; return as-is
+  if (messages[[1]][["content"]] == system_prompt) {
+    return(messages)
+  }
+
+  cli::cli_abort("`system_prompt` and `messages[[1]]` contained conflicting system prompts")
 }
+
+openai_check_conversation <- function(messages, allow_null = FALSE) {
+  if (is.null(messages) && isTRUE(allow_null)) {
+    return()
+  }
+
+  if (!is.list(messages) ||
+      !(is.null(names(messages)) || all(names(messages) == ""))) {
+    stop_input_type(
+      messages,
+      "an unnamed list of messages",
+      allow_null = FALSE
+    )
+  }
+
+  for (message in messages) {
+    if (!is.list(message) ||
+        !is.character(message$role)) {
+      cli::cli_abort("Each message must be a named list with at least a `role` field.")
+    }
+  }
+}
+
+new_openai_model <- function(base_url = "https://api.openai.com/v1",
+                             model = NULL,
+                             seed = NULL,
+                             extra_args = list(),
+                             api_key = openai_key(),
+                             error_call = caller_env()) {
+
+  # These checks could/should be placed in the validator, but the S7 object is
+  # currently an implementation detail. Keeping these errors here avoids
+  # leaking that implementation detail to the user.
+
+  check_string(base_url, call = error_call())
+  check_string(model, allow_null = TRUE, call = error_call())
+  check_number_whole(seed, allow_null = TRUE, call = error_call())
+  # check_named_list(extra_args, call = error_call())
+  check_string(api_key, call = error_call())
+
+  model <- model %||% "gpt-4o-mini"
+  if (is_testing() && is.null(seed)) {
+    seed <- 1014
+  }
+
+  openai_model(
+    base_url = base_url,
+    model = model,
+    seed = seed,
+    api_key = api_key,
+    extra_args = list()
+  )
+}
+
+openai_model <- new_class(
+  "openai_model",
+  package = "elmer",
+  properties = list(
+    base_url = class_character,
+    model = class_character,
+    seed = class_double,
+    api_key = class_character,
+    extra_args = class_list
+  )
+)
 
 openai_key_exists <- function() {
   key_exists("OPENAI_API_KEY")
@@ -124,4 +185,96 @@ openai_key_exists <- function() {
 
 openai_key <- function() {
   key_get("OPENAI_API_KEY")
+}
+
+# HTTP request and response handling -------------------------------------
+
+openai_chat_response <- function(mode = c("value", "stream", "async-stream", "async-value"),
+                                 model,
+                                 messages,
+                                 tools = list(),
+                                 extra_args = list()) {
+
+  mode <- arg_match(mode)
+  stream <- mode %in% c("stream", "async-stream")
+
+  req <- openai_chat_request(
+    model = model,
+    messages = messages,
+    tools = tools,
+    stream = stream,
+    extra_args = extra_args
+  )
+
+  switch(mode,
+    "value" = openai_chat_value(req),
+    "stream" = openai_chat_stream(req),
+    "async-value" = openai_chat_async_value(req),
+    "async-stream" = openai_chat_async_stream(req)
+  )
+}
+
+openai_chat_value <- function(req) {
+  resp_body_json(req_perform(req))
+}
+on_load(openai_chat_stream <- chat_stream(openai_stream_is_done, openai_stream_parse))
+
+openai_chat_async_value <- function(req) {
+  promises::then(req_perform_promise(req), resp_body_json)
+}
+on_load(openai_chat_async_stream <- chat_stream_async(openai_stream_is_done, openai_stream_parse))
+
+# https://platform.openai.com/docs/api-reference/chat/create
+openai_chat_request <- function(model,
+                                stream = TRUE,
+                                messages = list(),
+                                tools = list(),
+                                extra_args = list()) {
+
+  req <- request(model@base_url)
+  req <- req_url_path_append(req, "/chat/completions")
+  req <- req_auth_bearer_token(req, model@api_key)
+  req <- req_retry(req, max_tries = 2)
+  req <- req_error(req, body = function(resp) resp_body_json(resp)$error$message)
+
+  extra_args <- utils::modifyList(model@extra_args, extra_args)
+
+  data <- compact(list2(
+    messages = messages,
+    model = model@model,
+    seed = model@seed,
+    stream = stream,
+    tools = tools,
+    !!!extra_args
+  ))
+  req <- req_body_json(req, data)
+
+  req
+}
+
+openai_stream_is_done <- function(event) {
+  identical(event$data, "[DONE]")
+}
+openai_stream_parse <- function(event) {
+  jsonlite::parse_json(event$data)
+}
+openai_stream_text <- function(event, streaming) {
+  event$choices[[1]]$delta$content
+}
+openai_stream_message <- function(result) {
+  result$choices[[1]]$delta
+}
+openai_stream_merge_chunks <- function(cur, chunk) {
+  if (is.null(cur)) {
+    chunk
+  } else {
+    merge_dicts(cur, chunk)
+  }
+}
+
+openai_value_text <- function(event) {
+  event$choices[[1]]$message$content
+}
+openai_value_message <- function(result) {
+  result$choices[[1]]$message
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   rlang::run_on_load()
+  S7::methods_register()
 }
+
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
