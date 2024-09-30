17a1ce82ee640455aec4816ddadadb18468ee50f
Hadley Wickham <h.wickham@gmail.com>
2024-09-30 16:50:02 -0500

    Add text content

--- tests/testthat/test-content.R
+++ tests/testthat/test-content.R
@@ -4,7 +4,6 @@ test_that("invalid inputs give useful errors", {
   expect_snapshot(error = TRUE, {
     chat$chat(question = "Are unicorns real?")
     chat$chat(TRUE)
-    chat$chat(list())
     chat$chat(list(type = "unicorn"))
     chat$chat(list(type = "text"))
     chat$chat(list(type = "text", text = NULL))
@@ -12,50 +11,46 @@ test_that("invalid inputs give useful errors", {
 })
 
 test_that("inputs are validated", {
+  openai <- new_openai_provider(model = "gpt-4o")
 
-  expect_identical(
-    normalize_chat_input("Simple string"),
-    list(role = "user", content = "Simple string")
+  expect_equal(
+    normalize_content(openai, "Simple string"),
+    list(content_text("Simple string"))
   )
 
-  expect_identical(
-    normalize_chat_input(!!!letters[1:3]),
+  expect_equal(
+    normalize_content(openai, !!!letters[1:3]),
     list(
-      role = "user",
-      content = lapply(
-        letters[1:3],
-        function(x) list(type = "text", text = x)
-      )
+      content_text("a"),
+      content_text("b"),
+      content_text("c")
     )
   )
 
-  expect_identical(
-    normalize_chat_input(letters[1:3]),
+  expect_equal(
+    normalize_content(openai, letters[1:3]),
+    list(content_text("a\nb\nc"))
+  )
+
+  expect_equal(
+    normalize_content(openai, letters[1:3], letters[4:6]),
     list(
-      role = "user",
-      content = "a\nb\nc"
+      content_text("a\nb\nc"),
+      content_text("d\ne\nf")
     )
   )
 
-  expect_identical(
-    normalize_chat_input(letters[1:3], letters[4:6]),
+  expect_equal(
+    normalize_content(
+      openai,
+      "one",
+      list(type = "text", text = "two"),
+      content_text("three")
+    ),
     list(
-      role = "user",
-      content = list(
-        list(type = "text", text = "a\nb\nc"),
-        list(type = "text", text = "d\ne\nf")
-      )
-    )
-  )
-
-  expect_identical(
-    normalize_chat_input("one", "two", list(type = "text", text = "three")),
-    list(
-      role = "user",
-      content = lapply(
-        c("one", "two", "three"),
-        function(x) list(type = "text", text = x)
-      )
+      content_text("one"),
+      content_text("two"),
+      content_text("three")
     )
   )
 })
