766ada374fc537946f46cafe1a4804ecd2ca8cfd
Hadley Wickham <h.wickham@gmail.com>
2024-09-30 15:24:07 -0500

    Rename model to provider (#59)

--- R/api.R
+++ R/api.R
@@ -1,7 +1,7 @@
 # Currently performing chat request is not generic as there appears to
 # be sufficiently genericity elsewhere to handle the API variations.
 # We will recconsider this in the future if necessary.
-chat_perform <- function(model,
+chat_perform <- function(provider,
                          mode = c("value", "stream", "async-stream", "async-value"),
                          messages,
                          tools = list(),
@@ -11,7 +11,7 @@ chat_perform <- function(model,
   stream <- mode %in% c("stream", "async-stream")
 
   req <- chat_request(
-    model = model,
+    provider = provider,
     messages = messages,
     tools = tools,
     stream = stream,
@@ -19,18 +19,18 @@ chat_perform <- function(model,
   )
 
   switch(mode,
-    "value" = chat_perform_value(model, req),
-    "stream" = chat_perform_stream(model, req),
-    "async-value" = chat_perform_async_value(model, req),
-    "async-stream" = chat_perform_async_stream(model, req)
+    "value" = chat_perform_value(provider, req),
+    "stream" = chat_perform_stream(provider, req),
+    "async-value" = chat_perform_async_value(provider, req),
+    "async-stream" = chat_perform_async_stream(provider, req)
   )
 }
 
-chat_perform_value <- function(model, req) {
+chat_perform_value <- function(provider, req) {
   resp_body_json(req_perform(req))
 }
 
-on_load(chat_perform_stream <- coro::generator(function(model, req) {
+on_load(chat_perform_stream <- coro::generator(function(provider, req) {
   resp <- httr2::req_perform_connection(req)
   on.exit(close(resp))
   reg.finalizer(environment(), function(e) { close(resp) }, onexit = FALSE)
@@ -39,10 +39,10 @@ on_load(chat_perform_stream <- coro::generator(function(model, req) {
     event <- httr2::resp_stream_sse(resp)
     if (is.null(event)) {
       abort("Connection failed")
-    } else if (stream_is_done(model, event)) {
+    } else if (stream_is_done(provider, event)) {
       break
     } else {
-      yield(stream_parse(model, event))
+      yield(stream_parse(provider, event))
     }
   }
 
@@ -52,11 +52,11 @@ on_load(chat_perform_stream <- coro::generator(function(model, req) {
   }
 }))
 
-chat_perform_async_value <- function(model, req) {
+chat_perform_async_value <- function(provider, req) {
   promises::then(req_perform_promise(req), resp_body_json)
 }
 
-on_load(chat_perform_async_stream <- coro::async_generator(function(model, req, polling_interval_secs = 0.1) {
+on_load(chat_perform_async_stream <- coro::async_generator(function(provider, req, polling_interval_secs = 0.1) {
   resp <- req_perform_connection(req, blocking = FALSE)
   on.exit(close(resp))
   # TODO: Investigate if this works with async generators
@@ -67,10 +67,10 @@ on_load(chat_perform_async_stream <- coro::async_generator(function(model, req, 
     if (is.null(event)) {
       # TODO: Detect if connection is closed and stop polling
       await(coro::async_sleep(polling_interval_secs))
-    } else if (stream_is_done(model, event)) {
+    } else if (stream_is_done(provider, event)) {
       break
     } else {
-      yield(stream_parse(model, event))
+      yield(stream_parse(provider, event))
     }
   }
 
@@ -82,68 +82,68 @@ on_load(chat_perform_async_stream <- coro::async_generator(function(model, req, 
 
 # Create a request------------------------------------
 
-chat_request <- new_generic("chat_request", "model",
-  function(model, stream = TRUE, messages = list(), tools = list(), extra_args = list()) {
+chat_request <- new_generic("chat_request", "provider",
+  function(provider, stream = TRUE, messages = list(), tools = list(), extra_args = list()) {
     S7_dispatch()
   })
 
 # Extract data from streaming results ------------------------------------
 
-stream_is_done <- new_generic("stream_is_done", "model",
-  function(model, event) {
+stream_is_done <- new_generic("stream_is_done", "provider",
+  function(provider, event) {
     S7_dispatch()
   }
 )
-stream_parse <- new_generic("stream_parse", "model",
-  function(model, event) {
+stream_parse <- new_generic("stream_parse", "provider",
+  function(provider, event) {
     S7_dispatch()
   }
 )
-stream_text <- new_generic("stream_text", "model",
-  function(model, event) {
+stream_text <- new_generic("stream_text", "provider",
+  function(provider, event) {
     S7_dispatch()
   }
 )
-stream_merge_chunks <- new_generic("stream_merge_chunks", "model",
-  function(model, result, chunk) {
+stream_merge_chunks <- new_generic("stream_merge_chunks", "provider",
+  function(provider, result, chunk) {
     S7_dispatch()
   }
 )
-stream_message <- new_generic("stream_message", "model",
-  function(model, result) {
+stream_message <- new_generic("stream_message", "provider",
+  function(provider, result) {
     S7_dispatch()
   }
 )
 
 # Extract data from non-streaming results --------------------------------------
 
-value_text <- new_generic("value_text", "model",
-  function(model, event) {
+value_text <- new_generic("value_text", "provider",
+  function(provider, event) {
     S7_dispatch()
   }
 )
-value_message <- new_generic("value_message", "model",
-  function(model, result) {
+value_message <- new_generic("value_message", "provider",
+  function(provider, result) {
     S7_dispatch()
   }
 )
 
 # Tool calling -----------------------------------------------------------------
 
-value_tool_calls <- new_generic("value_tool_calls", "model",
-  function(model, message, tools) {
+value_tool_calls <- new_generic("value_tool_calls", "provider",
+  function(provider, message, tools) {
     S7_dispatch()
   }
 )
 
-call_tools <- new_generic("call_tools", "model",
-  function(model, tool_calls) {
+call_tools <- new_generic("call_tools", "provider",
+  function(provider, tool_calls) {
     S7_dispatch()
   }
 )
 
-call_tools_async <- new_generic("call_tools_async", "model",
-  function(model, tool_calls) {
+call_tools_async <- new_generic("call_tools_async", "provider",
+  function(provider, tool_calls) {
     S7_dispatch()
   }
 )
