6bf28b115c36d775673d0ebb995bdd6349e68405
Hadley Wickham <h.wickham@gmail.com>
2024-10-09 11:49:49 -0500

    Only need to test async code at Chat level (#109)

--- tests/testthat/test-provider-gemini.R
+++ tests/testthat/test-provider-gemini.R
@@ -2,14 +2,7 @@
 
 test_that("can make simple request", {
   chat <- chat_gemini("Be as terse as possible; no punctuation")
-  resp <- chat$chat("What is 1 + 1?")
-  expect_match(resp, "2")
-  expect_equal(chat$last_turn()@tokens, c(17, 1))
-})
-
-test_that("can make simple async request", {
-  chat <- chat_gemini("Be as terse as possible; no punctuation")
-  resp <- sync(chat$chat_async("What is 1 + 1?"))
+  resp <- chat$chat("What is 1 + 1?", echo = FALSE)
   expect_match(resp, "2")
   expect_equal(chat$last_turn()@tokens, c(17, 1))
 })
@@ -17,9 +10,6 @@ test_that("can make simple async request", {
 test_that("can make simple streaming request", {
   chat <- chat_gemini("Be as terse as possible; no punctuation")
   resp <- coro::collect(chat$stream("What is 1 + 1?"))
-  expect_match(paste0(unlist(resp), collapse = ""), "2")
-
-  resp <- sync(coro::async_collect(chat$stream_async("1 + 1")))
   expect_match(paste0(unlist(resp), collapse = ""), "2")
 })
 
