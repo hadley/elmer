023d36099545972f724b5f9b111fac02355db88f
Hadley Wickham <h.wickham@gmail.com>
2024-10-09 08:32:54 -0500

    Use for claude

--- tests/testthat/test-provider-claude.R
+++ tests/testthat/test-provider-claude.R
@@ -18,115 +18,31 @@ test_that("can make simple streaming request", {
   expect_match(paste0(unlist(resp), collapse = ""), "2")
 })
 
-test_that("system prompt can be passed explicitly or as a turn", {
-  system_prompt <- "Return very minimal output, AND ONLY USE UPPERCASE."
+# Common provider interface -----------------------------------------------
 
-  chat <- chat_claude(system_prompt = system_prompt)
-  resp <- chat$chat("What is the name of Winnie the Pooh's human friend?")
-  expect_match(resp, "CHRISTOPHER ROBIN")
-
-  chat <- chat_claude(turns = list(Turn("system", system_prompt)))
-  resp <- chat$chat("What is the name of Winnie the Pooh's human friend?")
-  expect_match(resp, "CHRISTOPHER ROBIN")
+test_that("defaults are reported", {
+  expect_snapshot(. <- chat_claude())
 })
 
-test_that("existing conversation history is used", {
-  chat <- chat_claude(turns = list(
-    Turn("system", "Return very minimal output; no punctuation."),
-    Turn("user", "List the names of any 8 of Santa's 9 reindeer."),
-    Turn("assistant", "Dasher, Dancer, Vixen, Comet, Cupid, Donner, Blitzen, and Rudolph.")
-  ))
-  expect_length(chat$turns(), 2)
+test_that("respects turns interface", {
+  chat_fun <- chat_claude
 
-  resp <- chat$chat("Who is the remaining one? Just give the name")
-  expect_equal(resp, "Prancer")
-  expect_length(chat$turns(), 4)
+  test_turns_system(chat_fun)
+  test_turns_existing(chat_fun)
 })
 
-# Tool calls -------------------------------------------------------------------
+test_that("all tool variations work", {
+  chat_fun <- chat_claude
 
-test_that("can make a simple tool call", {
-  chat <- chat_claude(system_prompt = "Be very terse, not even punctuation.")
-  chat$register_tool(ToolDef(
-    function() "2024-01-01",
-    name = "get_date",
-    description = "Gets the current date"
-  ))
-
-  result <- chat$chat("What's the current date in YMD format?")
-  expect_match(result, "2024-01-01")
-
-  result <- chat$chat("What month is it?")
-  expect_match(result, "January")
+  test_tools_simple(chat_fun)
+  test_tools_async(chat_fun)
+  test_tools_parallel(chat_fun)
+  test_tools_sequential(chat_fun, total_calls = 6)
 })
 
-test_that("can make an async tool call", {
-  chat <- chat_claude(system_prompt = "Be very terse, not even punctuation.")
-  chat$register_tool(ToolDef(
-    coro::async(function() "2024-01-01"),
-    name = "get_date",
-    description = "Gets the current date"
-  ))
+test_that("can use images", {
+  chat_fun <- chat_claude
 
-  result <- sync(chat$chat_async("What's the current date in YMD format?"))
-  expect_match(result, "2024-01-01")
-
-  expect_snapshot(chat$chat("Great. Do it again."), error = TRUE)
-})
-
-test_that("can call multiple tools in parallel", {
-  chat <- chat_claude(system_prompt = "Be very terse, not even punctuation.")
-  chat$register_tool(ToolDef(
-    function(person) if (person == "Joe") "sage green" else "red",
-    name = "favourite_color",
-    description = "Returns a person's favourite colour",
-    arguments = list(person = ToolArg("string", "Name of a person")),
-    strict = TRUE
-  ))
-
-  result <- chat$chat("
-    What are Joe and Hadley's favourite colours?
-    Answer like name1: colour1, name2: colour2
-  ")
-  expect_identical(result, "Joe: sage green, Hadley: red")
-  expect_length(chat$turns(), 4)
-})
-
-test_that("can call multiple tools in sequence", {
-  chat <- chat_claude(system_prompt = "Be very terse, not even punctuation.")
-  chat$register_tool(ToolDef(
-    function() 2024,
-    name = "get_year",
-    description = "Get the current year"
-  ))
-  chat$register_tool(ToolDef(
-    function(year) if (year == 2024) "Susan" else "I don't know",
-    name = "popular_name",
-    description = "Gets the most popular name for a year",
-    arguments = list(year = ToolArg("integer", "Year"))
-  ))
-
-  result <- chat$chat("What was the most popular name this year?")
-  expect_match(result, "Susan")
-  expect_length(chat$turns(), 6)
-})
-
-# Images -----------------------------------------------------------------
-
-test_that("can use inline images", {
-  chat <- chat_claude(model = "claude-3-5-sonnet-20240620")
-  response <- chat$chat(
-    "What's in this image? (Be sure to mention the outside shape)",
-    content_image_file(system.file("httr2.png", package = "elmer"))
-  )
-  expect_match(response, "hex")
-  expect_match(response, "baseball")
-  expect_length(chat$turns(), 2)
-
-  remote_image <- content_image_url("https://httr2.r-lib.org/logo.png")
-  expect_snapshot(
-    . <- chat$chat("What's in this image?", remote_image),
-    error = TRUE
-  )
-  expect_length(chat$turns(), 2)
+  test_images_inline(chat_fun)
+  test_images_remote_error(chat_fun)
 })
