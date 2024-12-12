dcf75b0d75a533350b0f699434601f85cc69c7f4
Hadley Wickham <h.wickham@gmail.com>
2024-12-12 12:06:30 -0600

    Automatically convert structured data to R types
    Fixes #136

--- R/types.R
+++ R/types.R
@@ -143,3 +143,38 @@ type_object <- function(.description = NULL,
     additional_properties = .additional_properties
   )
 }
+
+
+convert_from_type <- function(x, type) {
+  if (S7_inherits(type, TypeArray)) {
+    if (S7_inherits(type@items, TypeBasic)) {
+      switch(type@items@type,
+        boolean = as.logical(x),
+        integer = as.integer(x),
+        number = as.numeric(x),
+        string = as.character(x),
+        cli::cli_abort("Unknown type {type@items@type}", .internal = TRUE)
+      )
+    } else if (S7_inherits(type@items, TypeArray)) {
+      lapply(x, function(y) convert_from_type(y, type@items))
+    } else if (S7_inherits(type@items, TypeEnum)) {
+      factor(as.character(x), levels = type@items@values)
+    } else if (S7_inherits(type@items, TypeObject)) {
+      cols <- lapply(names(type@items@properties), function(name) {
+        vals <- lapply(x, function(y) y[[name]])
+        convert_from_type(vals, type_array(items = type@items@properties[[name]]))
+      })
+      names(cols) <- names(type@items@properties)
+      list2DF(cols)
+    } else {
+      x
+    }
+  } else if (S7_inherits(type, TypeObject)) {
+    out <- lapply(names(type@properties), function(name) {
+      convert_from_type(x[[name]], type@properties[[name]])
+    })
+    set_names(out, names(type@properties))
+  } else {
+    x
+  }
+}
